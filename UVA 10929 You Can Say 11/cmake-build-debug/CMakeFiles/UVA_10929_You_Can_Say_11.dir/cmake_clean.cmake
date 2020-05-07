9240ab7eb85315290ff0d942a2702f0d4e5dd21f
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2020-05-07 17:04:05 +0800

    Add files via upload

---
+++ UVA 10929 You Can Say 11/cmake-build-debug/CMakeFiles/UVA_10929_You_Can_Say_11.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_10929_You_Can_Say_11.dir/main.cpp.obj"
+  "UVA_10929_You_Can_Say_11.exe"
+  "UVA_10929_You_Can_Say_11.exe.manifest"
+  "UVA_10929_You_Can_Say_11.pdb"
+  "libUVA_10929_You_Can_Say_11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVA_10929_You_Can_Say_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
