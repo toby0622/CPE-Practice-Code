a728247f7fa548921bd5cfc97b25cd952c721361
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2020-12-14 16:41:05 +0800

    Finish

---
+++ UVA 11321 Sort Sort Sort/cmake-build-debug/CMakeFiles/UVA_11321_Sort_Sort_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_11321_Sort_Sort_Sort.dir/main.cpp.obj"
+  "UVA_11321_Sort_Sort_Sort.exe"
+  "UVA_11321_Sort_Sort_Sort.exe.manifest"
+  "UVA_11321_Sort_Sort_Sort.pdb"
+  "libUVA_11321_Sort_Sort_Sort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVA_11321_Sort_Sort_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
