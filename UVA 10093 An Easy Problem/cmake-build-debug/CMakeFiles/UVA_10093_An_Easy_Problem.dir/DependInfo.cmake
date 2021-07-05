d2ab7b98c535ed900d82d6c86d27e0d526b33883
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-07-05 11:12:45 +0800

    UVA 10093 Need Practice
    Code finished yet need more practice (not fully understand the question)

---
+++ UVA 10093 An Easy Problem/cmake-build-debug/CMakeFiles/UVA_10093_An_Easy_Problem.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/toby0/Downloads/CPE-Practice-Code/UVA 10093 An Easy Problem/main.cpp" "C:/Users/toby0/Downloads/CPE-Practice-Code/UVA 10093 An Easy Problem/cmake-build-debug/CMakeFiles/UVA_10093_An_Easy_Problem.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ UVA 10093 An Easy Problem/cmake-build-debug/CMakeFiles/UVA_10093_An_Easy_Problem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_10093_An_Easy_Problem.dir/main.cpp.obj"
+  "UVA_10093_An_Easy_Problem.exe"
+  "UVA_10093_An_Easy_Problem.exe.manifest"
+  "UVA_10093_An_Easy_Problem.pdb"
+  "libUVA_10093_An_Easy_Problem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVA_10093_An_Easy_Problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
