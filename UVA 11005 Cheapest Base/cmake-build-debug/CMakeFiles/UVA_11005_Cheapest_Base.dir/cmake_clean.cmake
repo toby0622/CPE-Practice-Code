2654a91217fa838f052d4d7a2ea24b0de3670463
Chi-Wen Teng <52705034+toby0622@users.noreply.github.com>
2021-07-06 14:57:59 +0800

    UVA 11005 Need Practice

---
+++ UVA 11005 Cheapest Base/cmake-build-debug/CMakeFiles/UVA_11005_Cheapest_Base.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_11005_Cheapest_Base.dir/main.cpp.obj"
+  "UVA_11005_Cheapest_Base.exe"
+  "UVA_11005_Cheapest_Base.exe.manifest"
+  "UVA_11005_Cheapest_Base.pdb"
+  "libUVA_11005_Cheapest_Base.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVA_11005_Cheapest_Base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ UVA 11005 Cheapest Base/cmake-build-debug/CMakeFiles/UVA_11005_Cheapest_Base.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UVA_11005_Cheapest_Base.
+# This may be replaced when dependencies are built.
