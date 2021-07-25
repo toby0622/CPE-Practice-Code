fb7fc57fb8d96b0a8c21edf3e92685bb8825499b
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2021-07-25 14:55:55 +0800

    UVA 10226 Need Practice

---
+++ UVA 10226 Hardwood Species/cmake-build-debug/CMakeFiles/UVA_10226_Hardwood_Species.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_10226_Hardwood_Species.dir/main.cpp.obj"
+  "UVA_10226_Hardwood_Species.exe"
+  "UVA_10226_Hardwood_Species.exe.manifest"
+  "UVA_10226_Hardwood_Species.pdb"
+  "libUVA_10226_Hardwood_Species.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVA_10226_Hardwood_Species.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ UVA 10226 Hardwood Species/cmake-build-debug/CMakeFiles/UVA_10226_Hardwood_Species.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UVA_10226_Hardwood_Species.
+# This may be replaced when dependencies are built.
