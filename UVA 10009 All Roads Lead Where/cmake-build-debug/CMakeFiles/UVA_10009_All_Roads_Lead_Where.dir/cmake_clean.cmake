1f9710ba8768b4f364569100d579a99f8eea9924
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2021-09-03 14:52:59 +0800

    UVA 10009 Need Practice

---
+++ UVA 10009 All Roads Lead Where/cmake-build-debug/CMakeFiles/UVA_10009_All_Roads_Lead_Where.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UVA_10009_All_Roads_Lead_Where.dir/main.cpp.obj"
+  "UVA_10009_All_Roads_Lead_Where.exe"
+  "UVA_10009_All_Roads_Lead_Where.exe.manifest"
+  "UVA_10009_All_Roads_Lead_Where.pdb"
+  "libUVA_10009_All_Roads_Lead_Where.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UVA_10009_All_Roads_Lead_Where.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ UVA 10009 All Roads Lead Where/cmake-build-debug/CMakeFiles/UVA_10009_All_Roads_Lead_Where.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UVA_10009_All_Roads_Lead_Where.
+# This may be replaced when dependencies are built.
